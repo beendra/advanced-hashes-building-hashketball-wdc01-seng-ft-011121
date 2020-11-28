a5434765c1f1c69746dd43280983d5549e79eda2
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 03:44:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,15 +25,16 @@ def game_hash
         slam_dunks: 7 
       },
       {player name: "Brook Lopez",
-        number: ,
-        shoe: ,
-        points: ,
-        rebounds: ,
-        assists: ,
-        steals: ,
-        blocks: ,
-        slam_dunks: 
+        number: 11,
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 15 
       },
+      
       ]
     }
   },

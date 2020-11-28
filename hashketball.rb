576b3255990ac6be536dd8c3a6a79a6f8d489fc9
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 03:43:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,16 +4,17 @@ def game_hash
     team_name: "Brooklyn Nets"
     colors: ["Black", "White"]
     players: [
-      {player name: ,
-        number: ,
-        shoe: ,
-        points: ,
-        rebounds: ,
-        assists: ,
-        steals: ,
-        blocks: ,
-        slam_dunks: 
+      {player name: "Alan Anderson",
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 1 
       },
+      
       ]
     }
   },

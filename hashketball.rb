b229de6c760446a7e607f998dd74843cac479f9a
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 04:01:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -92,6 +92,16 @@ def game_hash
         blocks: 5,
         slam_dunks: 5
       },
+      {player name: "Ben Gordon",
+        number: 8,
+        shoe: 15,
+        points: 33,
+        rebounds: 3,
+        assists: 2,
+        steals: 1,
+        blocks: 1,
+        slam_dunks: 0
+      },
       
       ]
       }

6edb45baeb75bbc7639472860659a0dae3300069
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 03:58:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -44,7 +44,16 @@ def game_hash
         blocks: 8,
         slam_dunks: 5 
       },
-      
+      {player name: "Jason Terry",
+        number: 31,
+        shoe: 15,
+        points: 19,
+        rebounds: 2,
+        assists: 2,
+        steals: 4,
+        blocks: 11,
+        slam_dunks: 1 
+      }
       ]
     }
   },
@@ -53,18 +62,19 @@ def game_hash
     team_name: "Charlotte Hornets"
     colors: ["Turquoise", "Purple"]
     players: [
-      {player name: ,
-        number: ,
-        shoe: ,
-        points: ,
-        rebounds: ,
-        assists: ,
-        steals: ,
-        blocks: ,
-        slam_dunks: 
+      {player name: "Jeff Adrien",
+        number: 4,
+        shoe: 18,
+        points: 10,
+        rebounds: 1,
+        assists: 1,
+        steals: 2,
+        blocks: 7,
+        slam_dunks: 2
       },
-      }
       
+      ]
+      }
       ]
     }
     }
